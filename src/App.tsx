0b5817d390fbd7441603ecc93736ab21feab1c53
Jingyu Xie (William) <bbsxjy@gmail.com>
2017-08-08 15:58:01 +0800

    Initial environment settings

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+            Hello Worldssss!!!
+         </div>
+      );
+   }
+}
+
+export default App;
