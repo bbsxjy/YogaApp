a39df728134e355d7d2b4d7746751c6050d5ad11
Jingyu Xie (William) <bbsxjy@gmail.com>
2017-08-14 16:25:42 +0800

    Finished Home landing page
    1. Use Landing page template and customized home page
    2. Add css, img, js to Assets folder

--- src/Root.tsx
+++ src/Root.tsx
@@ -3,6 +3,7 @@ import * as Routes from "./routes.ts";
 import {Route, Router} from "react-router";
 import createBrowserHistory from "history/createBrowserHistory";
 import {MainLayout} from "./pages/MainLayout.tsx";
+import {HomePage} from "./pages/home/HomePage.tsx";
 
 const browserHistory = createBrowserHistory();
 
@@ -10,9 +11,9 @@ export class Root extends React.Component {
     render() {
         return (
             <Router history={browserHistory}>
-                <Route path={Routes.home}>
-                    <Route exact path={Routes.home} component={MainLayout}/>
-                </Route>
+                <MainLayout>
+                    <Route exact path={Routes.home} component={HomePage}/>
+                </MainLayout>
             </Router>
         );
     }
---
+++ src/pages/home/HomePage.tsx
@@ -0,0 +1,107 @@
+import * as React from "react";
+import * as ReactRouter from "react-router";
+
+interface HomeProps extends ReactRouter.RouteComponentProps<any, {}> {}
+export class HomePage extends React.Component<HomeProps,{}> {
+    render () {
+        return (
+            <div>
+                <header className="masthead">
+                    <div className="container">
+                        <div className="intro-text">
+                            <div className="intro-lead-in">Life is all about Yoga!</div>
+                            <div className="intro-heading">圣玛瑜伽欢迎您</div>
+                            <a href="" className="btn btn-xl">点我预约课程</a>
+                        </div>
+                    </div>
+                </header>
+
+                <section className="bg-light" id="team">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-lg-12 text-center">
+                                <h2 className="section-heading">我们的团队</h2>
+                                <h3 className="section-subheading text-muted">Our Amazing Team</h3>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-sm-4">
+                                <div className="team-member">
+                                    <img className="mx-auto rounded-circle" src="../../../assets/img/team/1.jpg" alt=""/>
+                                        <h4>Kay Garland</h4>
+                                        <p className="text-muted">Lead Designer</p>
+                                        <ul className="list-inline social-buttons">
+                                            <li className="list-inline-item">
+                                                <a href="#">
+                                                    <i className="fa fa-twitter"/>
+                                                </a>
+                                            </li>
+                                            <li className="list-inline-item">
+                                                <a href="#">
+                                                    <i className="fa fa-facebook"/>
+                                                </a>
+                                            </li>
+                                            <li className="list-inline-item">
+                                                <a href="#">
+                                                    <i className="fa fa-linkedin"/>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                </div>
+                            </div>
+                            <div className="col-sm-4">
+                                <div className="team-member">
+                                    <img className="mx-auto rounded-circle" src="../../../assets/img/team/2.jpg" alt=""/>
+                                        <h4>Larry Parker</h4>
+                                        <p className="text-muted">Lead Marketer</p>
+                                        <ul className="list-inline social-buttons">
+                                            <li className="list-inline-item">
+                                                <a href="#">
+                                                    <i className="fa fa-twitter"/>
+                                                </a>
+                                            </li>
+                                            <li className="list-inline-item">
+                                                <a href="#">
+                                                    <i className="fa fa-facebook"/>
+                                                </a>
+                                            </li>
+                                            <li className="list-inline-item">
+                                                <a href="#">
+                                                    <i className="fa fa-linkedin"/>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                </div>
+                            </div>
+                            <div className="col-sm-4">
+                                <div className="team-member">
+                                    <img className="mx-auto rounded-circle" src="../../../assets/img/team/3.jpg" alt=""/>
+                                        <h4>Diana Pertersen</h4>
+                                        <p className="text-muted">Lead Developer</p>
+                                        <ul className="list-inline social-buttons">
+                                            <li className="list-inline-item">
+                                                <a href="#">
+                                                    <i className="fa fa-twitter"/>
+                                                </a>
+                                            </li>
+                                            <li className="list-inline-item">
+                                                <a href="#">
+                                                    <i className="fa fa-facebook"/>
+                                                </a>
+                                            </li>
+                                            <li className="list-inline-item">
+                                                <a href="#">
+                                                    <i className="fa fa-linkedin"/>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+
+        );
+    }
+}
