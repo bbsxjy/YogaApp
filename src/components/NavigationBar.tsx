195989bc9aeb6c65d971f97433e65410ec948177
Jingyu Xie (William) <bbsxjy@gmail.com>
2017-08-14 12:39:19 +0800

    Finished NavBar component
    1. Added fonts,bootstrap,fontawesome CDN to index.html.
    2. Completed NavBar component

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as ReactRouter from "react-router";
 import * as Routes from "../routes.ts";
-import {Navbar} from "react-bootstrap";
+import {Nav, Navbar, NavItem} from "react-bootstrap";
 import {Link} from "react-router-dom";
 
 interface NavigationBarProps extends ReactRouter.RouteComponentProps<any, {}> {
@@ -10,14 +10,30 @@ interface NavigationBarProps extends ReactRouter.RouteComponentProps<any, {}> {
 export class NavigationBar extends React.Component<NavigationBarProps, {}> {
     render() {
         return (
-            <Navbar fixedTop inverse fluid>
+            <Navbar fixedTop inverse fluid id="mainNav">
+
                 <Link to={Routes.home}>
                     <Navbar.Header>
-                        <Navbar.Brand>
-                            This is brand
-                        </Navbar.Brand>
+                        <Navbar.Brand>圣玛瑜伽</Navbar.Brand>
+                        <Navbar.Toggle data-toggle="collapse"
+                                       data-target="#navbarResponsive"
+                                       aria-controls="navbarResponsive"
+                                       aria-expanded="false"
+                                       aria-label="Toggle navigation"
+                                       className="navbar-toggler-right">
+                            菜单
+                            <i className="fa fa-bars"/>
+                        </Navbar.Toggle>
                     </Navbar.Header>
                 </Link>
+
+                <Nav className="ml-auto collapse navbar-collapse navbar-right"
+                     id="navbarResponsive">
+                    <NavItem href="#">课程表</NavItem>
+                    <NavItem href="#">预约课程</NavItem>
+                    <NavItem href="#">关于我们</NavItem>
+                </Nav>
+
             </Navbar>
         );
     }
